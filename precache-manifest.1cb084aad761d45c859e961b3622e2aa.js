25cabd2d1267877a35dda36eed922af97dab6af1
dnPC <denisagafonovwork.gmail.com>
2020-10-23 05:13:19 +0300

    Updates

---
+++ precache-manifest.1cb084aad761d45c859e961b3622e2aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876257c75cb4a3a1596a9c24aad84c3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "169f44d4207ba723bbc2",
+    "url": "/static/css/2.6b5155f9.chunk.css"
+  },
+  {
+    "revision": "5da4b166b209e043cfbe",
+    "url": "/static/css/main.3bdb8613.chunk.css"
+  },
+  {
+    "revision": "169f44d4207ba723bbc2",
+    "url": "/static/js/2.937f2b27.chunk.js"
+  },
+  {
+    "revision": "8d4a07d25a1f6e22b21f42b42f39523c",
+    "url": "/static/js/2.937f2b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da4b166b209e043cfbe",
+    "url": "/static/js/main.5dacb846.chunk.js"
+  },
+  {
+    "revision": "79bec8af708a68804254",
+    "url": "/static/js/runtime-main.4042a080.js"
+  }
+]);
